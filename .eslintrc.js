6584ba7d6877f132cdf48dfabe315a8c6715c698
Smoo-daishi <arakawa@smoo.co.jp>
2019-10-12 01:44:54 +0900

    master

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ["eslint:recommended"]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,52 @@
+/* eslint-disable no-undef */
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  entry: ["./src/js/app.js"],
+  watch: true,
+  mode: "development",
+  entry: "./src/js/app.js",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/public"
+    path: path.resolve(__dirname, "public/js"),
+    publicPath: "/js/"
   },
+  devServer: {
+    open: true,
+    port: 9000,
+    contentBase: "./public"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery"
+    })
+  ],
   module: {
+    // babel-loaderの設定
     rules: [
       {
         test: /\.js$/,
-        use: "babel-loader",
+        include: path.resolve(__dirname, "src/js"),
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-env", { modules: false }]]
+            }
+          }
+        ],
         exclude: /node_modules/
+      },
+      // CSS取り込み設定
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, "src/scss"),
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        include: path.resolve(__dirname, "src/images"),
+        loader: "url-loader"
       }
     ]
   }
